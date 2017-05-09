1ab36b831d2397a2940e52eb58ee5b625c276e11
Mathieu GOULIN <mathieu.goulin@gadz.org>
2017-05-09 17:40:35 +0200

    Add test to check if Utils constructor is private

--- src/test/java/info/kapable/utils/txttomail/UtilsTest.java
+++ src/test/java/info/kapable/utils/txttomail/UtilsTest.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.*;
 import info.kapable.utils.txttomail.Utils;
 
 import java.io.File;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
 
 import org.junit.Test;
 
@@ -18,4 +21,11 @@ public class UtilsTest {
 		assertTrue(Utils.basename(new File("/tmp/test.txt")).equals("test.txt"));
 	}
 
+	@Test
+	public void testConstructorIsPrivate() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+	  Constructor<Utils> constructor = Utils.class.getDeclaredConstructor();
+	  assertTrue(Modifier.isPrivate(constructor.getModifiers()));
+	  constructor.setAccessible(true);
+	  constructor.newInstance();
+	}
 }
